cbb163093aeef063fde97591a506b894b6d038bb
sreevidhya <tadigisreevidhya@gmail.com>
2026-01-14 12:34:40 +0530

    Airthmetic

--- Hello.java
+++ Hello.java
@@ -2,8 +2,17 @@
 { 
     public  static void main(String args[])
    {
-      int  x=9987;
-      byte b=(byte)x;
-      System.out.println(b);
+      int  num1=8;
+      //int  num2= 5;
+
+      //int result = num1 num2;
+      //num++;
+      int result1=num1++;
+      System.out.println(result1);
+
+      int num2 =8;
+      int result2= ++num2; 
+      System.out.println(result2) ;
+
    }
 }
