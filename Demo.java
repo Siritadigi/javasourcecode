2178b675a8ace0b8952e63cb38a0703060980ae8
sreevidhya <tadigisreevidhya@gmail.com>
2026-01-16 12:53:09 +0530

    relational

---
+++ Demo.java
@@ -0,0 +1,19 @@
+public class Demo
+{
+ public static void main(String args[])
+{
+    int x=10;
+    int y=8;
+     boolean result1 = x < y;
+     System.out.println(result1);
+     boolean result2= x >= y;
+     System.out.println(result2);
+     boolean result3= x==y;
+     System.out.println(result3);
+     boolean result4=x <= y;
+     System.out.println(result4);
+     boolean result5= x!=y;
+     System.out.println(result5);
+}
+}
+
