d7ae8914786502a4d4dcc4f0a4dc5242cd6605d9
sreevidhya <tadigisreevidhya@gmail.com>
2026-01-24 15:32:40 +0530

    conditional statements

---
+++ statements.java
@@ -0,0 +1,18 @@
+public class statements 
+{
+   public static void main (String args[] )
+ {
+   int x=16;
+   int y=6;
+   if(x>y || y>x)
+   {
+     System.out.println (x);
+     System.out.println("hello");
+   }
+    else
+    {
+        System.out.println(y);
+        System.out.println("thank you"); 
+    }
+ }
+}
