810d4792dc38fd163814260c2db7ebafcfa74ae7
sreevidhya <tadigisreevidhya@gmail.com>
2026-01-29 22:13:54 +0530

    loops

---
+++ loops.java
@@ -0,0 +1,42 @@
+public class loops 
+{
+    public static void main(String a[])
+  {
+       // int i=5;
+
+    //while(i<=4)
+    //{
+      //  System.out.println("Hello" + i);
+        //int j=1;
+        //while(j<=3)
+        //{
+           // System.out.println("hi" + j);
+            //j++;
+        //}
+      //i++;
+     //}
+
+     //System.out.println("bye");
+  
+  //do
+   
+     //System.out.println("hello" + i);
+   
+  //while(i<=3);
+  int i=1;
+  for(i=1;i<=5;i++)
+  {
+    System.out.println("DAY" + i);
+    int j=1;
+    for(j=1;j<=5;j++)
+    {
+        System.out.println(" " + (j+8) + "--" + (j+9));
+    }
+  }
+
+ }
+
+   
+}
+
+
